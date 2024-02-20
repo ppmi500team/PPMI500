4d2c60757d06e7decfe8fe760274480a019b7fd0
alex-reardon <arear001@ucr.edu>
2024-02-20 09:47:29 -0500

    edits to preprocessing.py

--- ppmi500/ppmi500/preprocessing.py
+++ ppmi500/ppmi500/preprocessing.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import boto3
+import json
+from io import StringIO
+
 
 def fill_missing_data_json(df, variable, df_col_name):
     """
@@ -127,12 +132,14 @@ def pop_missing_data(df, missing_info_df, df_col_name):
     return merged_df
 
 
-def add_qc_data(df):
+def add_qc_data(dir, df):
     """
     Add quality control (QC) data to DataFrame.
 
     Parameters:
+    - dir (str) : The directory path where the data files are located.
     - df (pandas.DataFrame): Input DataFrame.
+    
 
     Returns:
     pandas.DataFrame: DataFrame with QC data added.
@@ -144,8 +151,6 @@ def add_qc_data(df):
     df = convert_cols_to_string(df, ['subjectID','date'])
     merged_df = pd.merge(df, qc, how='left', on=['subjectID', 'date'])
     merged_df.drop_duplicates(inplace=True)
-    merged_df.to_csv('/Users/areardon/Desktop/x.csv')
-    print(merged_df)
     return merged_df
 
 
@@ -159,8 +164,10 @@ def get_metadata_info():
     s3 = boto3.client('s3')
     bucket_name = 'loni-data-curated-20230501'
     key = 'ppmi_500/curated/metadata/metadata.csv'
-    s3.download_file(bucket_name, key, dir + 'metadata.csv')
-    metadata = pd.read_csv(dir + 'metadata.csv')
+    # Read the CSV file from S3 into a DataFrame
+    csv_obj = s3.get_object(Bucket=bucket_name, Key=key)
+    csv_content = csv_obj['Body'].read().decode('utf-8')
+    metadata = pd.read_csv(StringIO(csv_content))
     metadata = metadata[['subjectIdentifier', 'researchGroup', 'visitIdentifier', 'subjectAge']]
     metadata.drop_duplicates(inplace=True)
     metadata = metadata[metadata['visitIdentifier'] == 'Baseline']
@@ -210,26 +217,15 @@ def merge_qc_2_antspymm(dir):
     merged_df['commonSex'] = merged_df['commonSex'].replace({'M': 'Male'})
 
     # Fill missing age and research group data with metadata info
-    metadata = get_metadata_info(dir)
+    metadata = get_metadata_info()
     merged_df = convert_cols_to_string(merged_df, ['age_BL', 'joinedDX'])
     metadata = convert_cols_to_string(metadata, ['subjectAge', 'researchGroup'])
     merged_df = fill_missing_data(merged_df, metadata, 'age_BL', 'subjectAge')
     merged_df = fill_missing_data(merged_df, metadata, 'joinedDX', 'researchGroup')
 
     # Add QC data
-    merged_df = add_qc_data(merged_df, dir)
+    merged_df = add_qc_data(dir, merged_df)
     merged_df = merged_df.drop_duplicates()
-
-    # Check for subids with missing DX, age, or sex info
-    missing_dx_subids = get_nan_subids(merged_df, 'joinedDX')
-    missing_age_subids = get_nan_subids(merged_df, 'age_BL')
-    missing_sex_subids = get_nan_subids(merged_df, 'commonSex')
-    missing_qc = get_nan_subids(merged_df, 'has_humanqc')
-    missing_modality = get_nan_subids(merged_df, 'modality')
-    print("missing qc" , missing_qc)
-    print("Missing DX subids:", missing_dx_subids)
-    print("Missing age: subids", missing_age_subids)
-    print("Missing sex subids:", missing_sex_subids)
-    print("missing modality", missing_modality)
-
     return merged_df
+
+
