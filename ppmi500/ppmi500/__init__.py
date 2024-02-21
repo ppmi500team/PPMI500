c3b1ec329a5b0584f13de5cc8171b23821c7c7af
Alex Reardon <arear001@ucr.edu>
2024-02-21 11:43:52 -0500

    Update __init__.py

--- ppmi500/ppmi500/__init__.py
+++ ppmi500/ppmi500/__init__.py
@@ -4,10 +4,9 @@ try:
 except:
     pass
 
-from .preprocessing import fill_missing_data
+from .preprocessing import pop_missing_data
 from .preprocessing import get_nan_subids
 from .preprocessing import convert_cols_to_string
-from .preprocessing import pop_missing_data
 from .preprocessing import add_qc_data
 from .preprocessing import merge_qc_2_antspymm
 
