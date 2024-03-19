96150b6e3486c400c37a03aba341ea033dd1e89f
Alex Reardon <arear001@ucr.edu>
2024-03-19 15:15:31 -0400

    Update test_process_qc.py

--- ppmi500/tests/test_process_qc.py
+++ ppmi500/tests/test_process_qc.py
@@ -4,6 +4,7 @@ qc_AR = '../ppmi500/data/QC/ppmi500_AR.csv'
 qc_BA = '../ppmi500/data/QC/ppmi500_BA.csv'
 qc_LF = '../ppmi500/data/QC/ppmi500_LF.csv'
 qc_XW = '../ppmi500/data/QC/ppmi500_XW.csv'
+ids_df = '../ppmi500/data/QC/ids_df.csv'
 
-df = ppmi500.process_qc_data(qc_AR, qc_LF, qc_BA, qc_XW)
-print(df)
+df = ppmi500.process_qc_data(qc_AR, qc_LF, qc_BA, qc_XW, ids_df)
+df.to_csv('/Users/areardon/Desktop/temp.csv')
